e0f595768ed0fcd0f31a20e0e5abdd8fb11b394b
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2023-04-15 18:27:11 -0600

    add: VariablesP1

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -2,6 +2,30 @@
 MMG*/
 public class HolaMundo {
    public static void main(String args[]){
-       System.out.println("Hola mundo");
+       /*String saludos = "Hola mundo";
+       var despedir = "Adios";//Intuye el tipo de variable
+       var num = 1;
+       
+       System.out.println(saludos);
+       System.out.println(saludos);
+       System.out.println(saludos);
+       System.out.println(despedir);
+       System.out.println(num);*/
+       
+       //concadenacion
+       var usuario = "Juan";
+       var saludo = "Hola";
+       
+       System.out.println(saludo+" "+usuario);
+       
+       var i  = 3;
+       var j = 5;
+       System.out.println(i+j);
+       System.out.println(saludo+i+j);
+       System.out.println(i+j+saludo);
+                
+       
+       
+       
    } 
 }
